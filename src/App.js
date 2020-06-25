0ce99a432ba4f19c8369823d280580bd359ba0b9
dharmadeveloper108 <whateverland108@gmail.com>
2020-06-25 22:25:29 +0200

    added geolocalization

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@ import React from 'react';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar.js';
+import apiConfig from './kuchika.js';
+
 
 class Container extends React.Component {
+
     constructor(props) {    
         super(props);    
         this.state = {      
             value: null,    
         };  
     }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition( (position) => {
+            alert("latitude is: " + position.coords.latitude + "\n" 
+                    + "longitude is: " + position.coords.longitude);
+            
+            const temperature_url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${apiConfig.weathe_api_key}&lat={lat}&lon={lon}&cnt=1`;
+            const uvindex_url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${apiConfig.weathe_api_key}&lat={lat}&lon={lon}&cnt=1`;
+            const humidity_url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${apiConfig.weathe_api_key}&lat={lat}&lon={lon}&cnt=1`;
+
+        })
+    }
+
+// getUVindex = () => {
+
+//     fetch(uvindex_url)
+//     .then(res => res.json())
+//     .then(data => console.log("Data loaded", data.list))
+// }
+
     render() {
+
            return(
             <div>
                 <Navbar/>
-                <div class="container">
+                <div className="container">
                     <Row/>
                 </div>
             </div>
@@ -23,14 +47,14 @@ class Container extends React.Component {
 }
 
 class Row extends React.Component {
-    renderColumn(x) {
+    renderColumn() {
         return(
             <Column/>
         );
     }
     render() {
         return(
-            <div class="row">
+            <div className="row">
                 {this.renderColumn()}
                 {this.renderColumn()}
             </div>
@@ -41,11 +65,12 @@ class Row extends React.Component {
 class Column extends React.Component {
     render() {
         return(
-            <div class="col">
+            <div className="col">
                 <h1>Hello world!</h1>
             </div>
         );
     }
 }
+
 
 export default Container;
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Navbar = () => {
     return (            <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">VATA WEATHER</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">VATA WEATHER</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Temperature </a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Temperature </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">UV Index</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">UV Index</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Humidity</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Humidity</a>
                     </li>
                     </ul>
                 </div>
